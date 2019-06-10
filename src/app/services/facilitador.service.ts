176222f14d1b1494e7146acb5f1d970d4929068b
Sebastian Uribe Alvarez <seuribe@bancolombia.com.co>
2019-06-10 16:17:56 -0500

    update

--- src/app/services/facilitador.service.ts
+++ src/app/services/facilitador.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class FacilitadorService {
-  url = "ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion"
+  url = "http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion"
   constructor(private http: HttpClient) { }
 
   getFacilitadores(celula: string): Observable<any> {
@@ -30,38 +30,38 @@ export class FacilitadorService {
   }
 
   guardar(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas', data);
     //alert("User created successfully.");
   }
 
 
   guardarP2(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas2', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas2', data);
     //alert("User created successfully.");
   }
 
   guardarP3(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas3', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas3', data);
     //alert("User created successfully.");
   }
 
   guardarP4(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas4', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas4', data);
     //alert("User created successfully.");
   }
 
   guardarP5(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas5', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas5', data);
     //alert("User created successfully.");
   }
 
   guardarP6(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas6', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntas6', data);
     //alert("User created successfully.");
   }
 
   guardarGeneral(data: any) {
-    return this.http.post('ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntasgeneral', data);
+    return this.http.post('http://ec2-3-14-3-164.us-east-2.compute.amazonaws.com:8080/valoracion/preguntasgeneral', data);
     //alert("User created successfully.");
   }
 }
